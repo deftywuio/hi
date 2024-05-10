e98c38918717c83e4c285e9ca46b8457189fda90
deftywuio <msjcariojsierra@hotmail.com>
2024-05-11 06:39:05 +0700

    test

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function levelOrderBottom(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.unshift(currentLevel);
+function numDistinct(s, t) {
+  const m = s.length;
+  const n = t.length;
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = 1;
   }
-  return result;
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (s[i - 1] === t[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+  return dp[m][n];
 }
