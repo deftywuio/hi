3a963845ded84eba819c8829dd86edefd8d595cd
deftywuio <msjcariojsierra@hotmail.com>
2024-05-11 23:57:53 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-function numDistinct(s, t) {
-  const m = s.length;
-  const n = t.length;
-  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-  for (let i = 0; i <= m; i++) {
-    dp[i][0] = 1;
-  }
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (s[i - 1] === t[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-      } else {
-        dp[i][j] = dp[i - 1][j];
+const combSort = (arr) => {
+  const shrinkFactor = 1.3;
+  let gap = arr.length;
+  let swapped = true;
+  while (gap > 1 || swapped) {
+    gap = Math.floor(gap / shrinkFactor);
+    if (gap < 1) {
+      gap = 1;
+    }
+    swapped = false;
+    let i = 0;
+    while (i + gap < arr.length) {
+      if (arr[i] > arr[i + gap]) {
+        [arr[i], arr[i + gap]] = [arr[i + gap], arr[i]];
+        swapped = true;
       }
+      i++;
     }
   }
-  return dp[m][n];
-}
+  return arr;
+};
